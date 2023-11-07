6358ead46eb1af26cc9f0e9c79fdd5b855bf5557
Eduardo Cruz <sencho1074@gmail.com>
2023-11-06 22:50:22 -0700

    minesweeper.py modified

--- minesweeper.py
+++ minesweeper.py
@@ -1,1 +1,19 @@
-print("hello World")
+from tkinter import *
+
+root = Tk()
+
+
+root.configure(bg="black")
+root.geometry('1440x720')
+root.title("Minesweeper Game")
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='red',  # change later to black
+    width=1440,
+    height=180
+)
+
+# Run the window
+root.mainloop()
