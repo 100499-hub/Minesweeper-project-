4fbb922ee7b8caf9bb498c591851841f1304e51e
Eduardo Cruz <sencho1074@gmail.com>
2023-11-07 18:50:00 -0700

    minesweeper.py modified

--- minesweeper.py
+++ minesweeper.py
@@ -1,18 +1,41 @@
 from tkinter import *
+import settings
+import utils
 
 root = Tk()
 
 
 root.configure(bg="black")
-root.geometry('1440x720')
+root.geometry(f'{settings.width}x{settings.height}')
 root.title("Minesweeper Game")
 root.resizable(False, False)
 
 top_frame = Frame(
     root,
-    bg='red',  # change later to black
-    width=1440,
-    height=180
+    bg='black',
+    width=settings.width,
+    height=utils.heigth_prct(25)
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(25),
+    height=utils.heigth_prct(75)
+)
+
+left_frame.place(x=0, y=utils.heigth_prct(25))
+
+center_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(75),
+    height=utils.heigth_prct(75)
+)
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.heigth_prct(25)
 )
 
 # Run the window
